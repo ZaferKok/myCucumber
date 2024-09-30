a44ea25f69c26ce1d7387b50342d43db8e30edcf
ZaferKok <62161849+ZaferKok@users.noreply.github.com>
2024-09-30 17:34:08 +0200

    Update LoginSteps.java

--- src/test/java/stepdefinitions/LoginSteps.java
+++ src/test/java/stepdefinitions/LoginSteps.java
@@ -5,18 +5,26 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class LoginSteps {
-    @When("kullanici adi girilir {string}")
-    public void kullaniciAdiGirilir(String kullaniciAdi) {
-       // kullaniciBox.sendKeys(kullaniciAdi);
 
+    @Given("the user is on the login page")
+    public void theUserIsOnTheLoginPage() {
+       driver.get("https://example.com/login");
+    }    
+    
+    @When("the user enters valid username {string}")
+    public void theUserEntersValidUsername(String username) {
+       usernameBox.sendKeys(username);
     }
 
-    @And("kulllanici sifresi girilir {string}")
-    public void kulllaniciSifresiGirilir(String password) {
-       // passwordBox.sendKeys(password);
+    @And("the user enters valid password {string}")
+    public void theUserEntersValidPassword(String password) {
+       passwordBox.sendKeys(password);
     }
 
-    @Then("login buton tiklanir")
-    public void loginButonTiklanir() {
+    @Then("the user is redirected to the homepage {string}")
+    public void theUserIsRedirectedToTheHomepage(String username) {
+        String ActualTitle = driver.getTitle();
+        String ExpectedTitle = "Welcome {username}";
+        Assert.assertEquals(ExpectedTitle, ActualTitle);
     }
 }
