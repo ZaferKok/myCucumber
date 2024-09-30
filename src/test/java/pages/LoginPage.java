a3b79e03eefe06fb2fdd8be419605f0b40f1c7b0
ZaferKok <62161849+ZaferKok@users.noreply.github.com>
2024-09-30 17:39:06 +0200

    Update LoginPage.java

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -1,22 +1,18 @@
 package pages;
 
-
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utilities.Driver;
 
-public class FHCLoginPage {
-    public FHCLoginPage(){
+public class LoginPage {
+    
+    public LoginPage(){PageFactory.initElements(Driver.getDriver(),this);}
 
-        PageFactory.initElements(Driver.getDriver(),this);
-    }
     @FindBy(id = "UserName")
     public WebElement username;
     @FindBy(id = "Password")
     public WebElement password;
-    @FindBy(xpath = "//input[@type='submit']")
-    public WebElement login;
-//    @FindBy (xpath = "//div[@id='divMessageResult']")
-//    public  WebElement errorMessage;
+    @FindBy(xpath = "//input[@name='Message']")
+    public WebElement Message;
 }
